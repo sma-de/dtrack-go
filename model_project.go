c5c1c3d46b4370044cab7dadbb6a1b1894ab2eb3
Armin Sczuka <armin.sczuka@sma.de>
2024-06-27 15:57:22 +0200

    workaround deserialization of time.Time, using string

--- model_project.go
+++ model_project.go
@@ -9,35 +9,32 @@
 
 package swagger
 
-import (
-	"time"
-)
-
 type Project struct {
-	Author string `json:"author,omitempty"`
-	Publisher string `json:"publisher,omitempty"`
-	Manufacturer *OrganizationalEntity `json:"manufacturer,omitempty"`
-	Supplier *OrganizationalEntity `json:"supplier,omitempty"`
-	Group string `json:"group,omitempty"`
-	Name string `json:"name,omitempty"`
-	Description string `json:"description,omitempty"`
-	Version string `json:"version,omitempty"`
-	Classifier string `json:"classifier,omitempty"`
-	Cpe string `json:"cpe,omitempty"`
-	Purl *PackageUrl `json:"purl,omitempty"`
-	SwidTagId string `json:"swidTagId,omitempty"`
-	DirectDependencies string `json:"directDependencies,omitempty"`
-	Uuid string `json:"uuid"`
-	Parent *Project `json:"parent,omitempty"`
-	Children []Project `json:"children,omitempty"`
-	Properties []ProjectProperty `json:"properties,omitempty"`
-	Tags []Tag `json:"tags,omitempty"`
-	LastBomImport time.Time `json:"lastBomImport,omitempty"`
-	LastBomImportFormat string `json:"lastBomImportFormat,omitempty"`
-	LastInheritedRiskScore float64 `json:"lastInheritedRiskScore,omitempty"`
-	Active bool `json:"active,omitempty"`
-	ExternalReferences []ExternalReference `json:"externalReferences,omitempty"`
-	Metadata *ProjectMetadata `json:"metadata,omitempty"`
-	Versions []ProjectVersion `json:"versions,omitempty"`
-	Metrics *ProjectMetrics `json:"metrics,omitempty"`
+	Author             string                `json:"author,omitempty"`
+	Publisher          string                `json:"publisher,omitempty"`
+	Manufacturer       *OrganizationalEntity `json:"manufacturer,omitempty"`
+	Supplier           *OrganizationalEntity `json:"supplier,omitempty"`
+	Group              string                `json:"group,omitempty"`
+	Name               string                `json:"name,omitempty"`
+	Description        string                `json:"description,omitempty"`
+	Version            string                `json:"version,omitempty"`
+	Classifier         string                `json:"classifier,omitempty"`
+	Cpe                string                `json:"cpe,omitempty"`
+	Purl               *PackageUrl           `json:"purl,omitempty"`
+	SwidTagId          string                `json:"swidTagId,omitempty"`
+	DirectDependencies string                `json:"directDependencies,omitempty"`
+	Uuid               string                `json:"uuid"`
+	Parent             *Project              `json:"parent,omitempty"`
+	Children           []Project             `json:"children,omitempty"`
+	Properties         []ProjectProperty     `json:"properties,omitempty"`
+	Tags               []Tag                 `json:"tags,omitempty"`
+	LastBomImport      string                `json:"lastBomImport,omitempty"`
+	//LastBomImport time.Time `json:"lastBomImport,omitempty"`
+	LastBomImportFormat    string              `json:"lastBomImportFormat,omitempty"`
+	LastInheritedRiskScore float64             `json:"lastInheritedRiskScore,omitempty"`
+	Active                 bool                `json:"active,omitempty"`
+	ExternalReferences     []ExternalReference `json:"externalReferences,omitempty"`
+	Metadata               *ProjectMetadata    `json:"metadata,omitempty"`
+	Versions               []ProjectVersion    `json:"versions,omitempty"`
+	Metrics                *ProjectMetrics     `json:"metrics,omitempty"`
 }
